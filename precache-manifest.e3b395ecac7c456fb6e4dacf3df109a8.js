d58a2a1326f6c2934c430efb1cc86d140e72c913
Andreitsiv <andreitsivwork@gmail.com>
2020-08-12 23:23:23 +0300

    Updates

---
+++ precache-manifest.e3b395ecac7c456fb6e4dacf3df109a8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad2e0c478c3b465dd5ba75c07932849",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "2fac9b83d314ba52cb35",
+    "url": "/deploy/static/css/main.8a8208eb.chunk.css"
+  },
+  {
+    "revision": "72f1e7cb5faf5b47ac9b",
+    "url": "/deploy/static/js/2.c1a18349.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/deploy/static/js/2.c1a18349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fac9b83d314ba52cb35",
+    "url": "/deploy/static/js/main.919e3e9b.chunk.js"
+  },
+  {
+    "revision": "db75d74611ceba7383cf",
+    "url": "/deploy/static/js/runtime-main.725dcda5.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/deploy/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "79a48893b30de93ebfa045c66386d251",
+    "url": "/deploy/static/media/banner_mobile.79a48893.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/deploy/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/deploy/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "3a41f082d05e2a2756c67f67f21b2324",
+    "url": "/deploy/static/media/photo-cover.3a41f082.svg"
+  }
+]);
