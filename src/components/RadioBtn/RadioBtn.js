15b6d648601693bddad19aecd6196966c4c50292
Andreitsiv <andreitsivwork@gmail.com>
2020-08-18 11:14:05 +0300

    changes in input type readio

--- src/components/RadioBtn/RadioBtn.js
+++ src/components/RadioBtn/RadioBtn.js
@@ -1,28 +1,35 @@
 import React from "react";
+import cn from "classnames"
 
 import "./Radio.scss"
 
 
-const RadioBtn = ({positions,inputRef,formChangeHandler,name,errors}) => {
-    return (
-        <div className="radio">
-            <label className={`inputLabel radio_label`}>Select your position</label>
-            <div>
-            {positions.map((position, index) => {
-                return (
-                    <div key={index} className="radio_btn">
-                        <input className="custom-radio"
-                            ref={inputRef} type="radio"
-                                onChange={formChangeHandler}
-                                name={name}
-                                value={position.id}/>
-                        <label className="label" id="radio-btn" htmlFor={positions.id}>{position.name}</label>
-                    </div>)
-            })}
-                <span className="inputError">{errors}</span></div>
+const RadioBtn = ({positions, inputRef, formChangeHandler, name, errors}) => {
 
+    const classes = cn(
+        "inputLabel",
+        "radio_label",
+    )
+    return (<>
+            <div className={`radio ${!!errors && "error"}`}>
+                <label className={classes}>Select your position</label>
+                <div>
+                    {positions.map((position, index) => {
+                        return (
+                            <div key={index} className="radio_btn">
+                                <input className="custom-radio"
+                                       ref={inputRef} type="radio"
+                                       onChange={formChangeHandler}
+                                       name={name}
+                                       value={position.id}/>
+                                <label className="radio-item__label" id="radio-btn"
+                                       htmlFor={positions.id}>{position.name}</label>
+                            </div>)
+                    })}
 
-        </div>
+                </div>
+            </div>
+            <span className="inputError">{errors}</span></>
     )
 }
 export default RadioBtn
